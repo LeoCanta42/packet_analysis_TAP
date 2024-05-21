1e145226555a9bc55dbd0704a90b76f0a1d1061f
Leonardo Cantarella <leonardo@cantarella.it>
2024-05-21 15:59:36 +0200

    chore: added spark submit. To work on packet analyzer

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Start
 
 sudo tshark -i 'dev' -l -n -x -T json > packets.log
+
+
+tested with versions:
+logstash 8.13.2
+zookeper after 7.6.1
+cp-kafka after 7.6.1
+kafka-ui after 0.7.2
+spark-py after 3.4.0
--- packet_analyzer.py
+++ packet_analyzer.py
@@ -1,26 +1,16 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, concat, lit
 
-scala_version = '2.12'  # TODO: Ensure this is correct
-spark_version = '3.2.1'
-packages = [
-    f'org.apache.spark:spark-sql-kafka-0-10_{scala_version}:{spark_version}',
-    'org.apache.kafka:kafka-clients:3.2.0'
-]
-spark = SparkSession.builder\
-   .master("local")\
-   .appName("kafka-example")\
-   .config("spark.jars.packages", ",".join(packages))\
-   .getOrCreate()
+print("Starting packet analyzer...")
+# Initialize SparkSession
+spark = SparkSession.builder.appName("KafkaSparkIntegration").getOrCreate()
 
-def main():
-    kafkaDf = spark.read.format("kafka")\
-    .option("kafka.bootstrap.servers", "PLAINTEXT://kafkaserver:9092")\
-    .option("subscribe", "packets")\
-    .option("startingOffsets", "earliest")\
+# Read from Kafka
+df = spark.readStream.format("kafka") \
+    .option("kafka.bootstrap.servers", "kafkaserver:9092") \
+    .option("subscribe", "packets") \
     .load()
-    kafkaDf.select(
-        concat(col("topic"), lit(':'), col("partition").cast("string")).alias("topic_partition"),
-        col("offset"),
-        col("value").cast("string")
-    ).show()
+
+# Process the data (this is a simple example)
+df = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+
+df.writeStream.format("console").start().awaitTermination()
