3cc5cff9aa8cac035f21617300450f5c57d68687
Leonardo Cantarella <leonardo@cantarella.it>
2024-05-28 14:37:19 +0200

    fix: using official apache/spark-py

--- packet_analyzer.py
+++ packet_analyzer.py
@@ -4,12 +4,16 @@ print("*** Starting packet analyzer...\n\n")
 # Initialize SparkSession
 spark = SparkSession.builder.appName("KafkaSparkIntegration").getOrCreate()
 
+spark.sparkContext.setLogLevel("ERROR")
+
 # Read from Kafka
 df = spark.readStream.format("kafka") \
     .option("kafka.bootstrap.servers", "kafkaserver:9092") \
     .option("subscribe", "packets") \
     .load()
 
-print(df)
+#print packets to console
+query = df.writeStream.outputMode("append").format("console").start()
+query.awaitTermination()
 
 print("*** Closing packet analyzer...\n\n")
