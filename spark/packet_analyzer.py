c5285679271faf65ef9400065c496cf3a4ccc49a
Leonardo Cantarella <leonardo@cantarella.it>
2024-06-30 13:49:11 +0200

    working on training model (maybe working)

--- spark/packet_analyzer.py
+++ spark/packet_analyzer.py
@@ -1,25 +1,17 @@
 from pyspark.sql import SparkSession
 from pyspark.conf import SparkConf
 from pyspark.sql.functions import col, from_json
-from pyspark.sql.types import StructType, StructField, StringType, ArrayType, IntegerType
+from pyspark.sql.types import StructType, StructField, StringType, ArrayType
 import geoip2.database
 import requests
-import numpy as np
 from ipaddress import ip_address, ip_network
-import pickle
+import dns.resolver
+from joblib import load
+
 
 ################################
 # Anomaly detection with model #
 ################################
-
-# Load the trained model
-with open("anomaly_detection_model.pkl", "rb") as f:
-    clf = pickle.load(f)
-
-# Define a function to predict anomalies
-def detect_anomaly(*cols):
-    X = np.column_stack(cols)
-    return clf.predict(X)
 
 ################################
 #   IP type classification     #
@@ -61,56 +53,6 @@ def ip_to_int(ip):
         return None
 
 ################################
-#    Defining packet schema    #
-################################
-
-# Define the schema
-frame_schema = StructType([
-        StructField("frame_frame_time", StringType(), True),
-        StructField("frame_frame_number", StringType(), True),
-        StructField("frame_frame_len", StringType(), True),
-        StructField("frame_frame_protocols", StringType(), True)
-    ])
-
-eth_schema = StructType([
-    StructField("eth_eth_src", StringType(), True),
-    StructField("eth_eth_dst", StringType(), True),
-    StructField("eth_eth_type", StringType(), True)
-])
-
-ip_schema = StructType([
-    StructField("ip_ip_src", StringType(), True),
-    StructField("ip_ip_dst", StringType(), True),
-    StructField("ip_ip_proto", StringType(), True)
-])
-
-udp_schema = StructType([
-    StructField("udp_udp_srcport", StringType(), True),
-    StructField("udp_udp_dstport", StringType(), True)
-])
-
-dns_schema = StructType([
-    StructField("dns_dns_qry_name", StringType(), True),
-    StructField("dns_dns_qry_type", StringType(), True),
-    StructField("dns_dns_qry_class", StringType(), True),
-    StructField("dns_dns_resp_name", ArrayType(StringType()), True),
-    StructField("dns_dns_resp_type", ArrayType(StringType()), True),
-    StructField("dns_dns_resp_class", ArrayType(StringType()), True)
-])
-
-# Define the root schema
-schema = StructType([
-    StructField("timestamp", StringType(), True),
-    StructField("layers", StructType([
-        StructField("frame", frame_schema, True),
-        StructField("eth", eth_schema, True),
-        StructField("ip", ip_schema, True),
-        StructField("udp", udp_schema, True),
-        StructField("dns", dns_schema, True)
-    ]), True)
-])
-
-################################
 #        Geolocation           #
 ################################
 # Take the geolocalization from the IP using local GeoLite2-DB
@@ -119,7 +61,7 @@ def get_geolocation_geo2(ip):
         return "Special or private IP Address - No Geolocation"
     else:
         try:
-            reader = geoip2.database.Reader('GeoLite2-Country.mmdb')
+            reader = geoip2.database.Reader('GeoLite2-City.mmdb')
             response = reader.country(ip)
             return response.country.name
         except:
@@ -148,12 +90,104 @@ def check_ip_threat(ip):
     response = requests.get(url, headers=headers)
     return response.json()
     
+################################
+#       Hostname resolver      #
+################################
+
+# Resolve IP addresses to hostnames
+def resolve_hostname(ip):
+    try:
+        return dns.resolver.resolve(ip, 'PTR')[0].to_text()
+    except:
+        return "Unknown"
 
 ################################
-#       Global variables       #
+#    Application detection     #
 ################################
 
-elastic_index = "packets"
+# Application protocol detetion
+def application_detection(port):
+    # Dictionary mapping ports to application protocols
+    port_protocol_map = {
+        20: 'FTP (Data)',
+        21: 'FTP (Control)',
+        22: 'SSH',
+        23: 'Telnet',
+        25: 'SMTP',
+        53: 'DNS',
+        80: 'HTTP',
+        110: 'POP3',
+        143: 'IMAP',
+        443: 'HTTPS',
+        587: 'SMTP (Secure)',
+        993: 'IMAP (Secure)',
+        995: 'POP3 (Secure)',
+        3306: 'MySQL',
+        3389: 'RDP',
+        5432: 'PostgreSQL',
+        5900: 'VNC',
+        6379: 'Redis',
+        8080: 'HTTP (Alternate)',
+    }
+    
+    # Retrieve the protocol for the given port or return 'Unknown' if not found
+    return port_protocol_map.get(port, 'Unknown')
+
+################################
+#    Defining packet schema    #
+################################
+
+# Define the schema
+frame_schema = StructType([
+        StructField("frame_frame_time", StringType(), True),
+        StructField("frame_frame_number", StringType(), True),
+        StructField("frame_frame_len", StringType(), True),
+        StructField("frame_frame_protocols", StringType(), True)
+    ])
+
+eth_schema = StructType([
+    StructField("eth_eth_src", StringType(), True),
+    StructField("eth_eth_dst", StringType(), True),
+    StructField("eth_eth_type", StringType(), True)
+])
+
+ip_schema = StructType([
+    StructField("ip_ip_src", StringType(), True),
+    StructField("ip_ip_dst", StringType(), True),
+    StructField("ip_ip_proto", StringType(), True)
+])
+
+udp_schema = StructType([
+    StructField("udp_udp_srcport", StringType(), True),
+    StructField("udp_udp_dstport", StringType(), True)
+])
+
+tcp_schema = StructType([
+    StructField("tcp_tcp_srcport", StringType(), True),
+    StructField("tcp_tcp_dstport", StringType(), True)
+])
+
+dns_schema = StructType([
+    StructField("dns_dns_qry_name", StringType(), True),
+    StructField("dns_dns_qry_type", StringType(), True),
+    StructField("dns_dns_qry_class", StringType(), True),
+    StructField("dns_dns_resp_name", ArrayType(StringType()), True),
+    StructField("dns_dns_resp_type", ArrayType(StringType()), True),
+    StructField("dns_dns_resp_class", ArrayType(StringType()), True)
+])
+
+# Define the root schema
+schema = StructType([
+    StructField("timestamp", StringType(), True),
+    StructField("layers", StructType([
+        StructField("frame", frame_schema, True),
+        StructField("eth", eth_schema, True),
+        StructField("ip", ip_schema, True),
+        StructField("udp", udp_schema, True),
+        StructField("tcp", tcp_schema, True),
+        StructField("dns", dns_schema, True)
+    ]), True)
+])
 
 
 ################################
@@ -162,88 +196,66 @@ elastic_index = "packets"
 
 def main():    
 
-    # Elastic conf
+    # ELASTIC CONF
+    elastic_index = "packets"
     sparkConf = SparkConf().set("es.nodes", "elasticsearch") \
                             .set("es.port", "9200")
 
-    # Initialize SparkSession
+    # INITIALIZE SPARKSESSION
     spark = SparkSession.builder.appName("KafkaSparkIntegration").config(conf=sparkConf).getOrCreate()
-
     spark.sparkContext.setLogLevel("ERROR")
 
-    # Read from Kafka
+    # READ FROM KAFKA
     df = spark.readStream.format("kafka") \
         .option("kafka.bootstrap.servers", "kafkaserver:9092") \
         .option("subscribe", "packets") \
         .load()
 
-    # Parse the JSON
+    # PARSE THE JSON
     df = df.selectExpr("CAST(timestamp AS STRING) AS timestamp", "CAST(value AS STRING) AS value")
     df = df.select(from_json(col("value"), schema).alias("data")).select("data.*")
 
-    # Take position from the IP
-
-    get_geolocation_udf = spark.udf.register("get_geolocation", get_geolocation_geo2)
-
+    # TAKE POSITION FROM THE IP
+    get_geolocation_udf = spark.udf.register("get_geolocation", get_geolocation_dbip)
     df = df.withColumn("src_position", get_geolocation_udf(col("layers.ip.ip_ip_src")))
     df = df.withColumn("dst_position", get_geolocation_udf(col("layers.ip.ip_ip_dst")))
 
-    # Check if the IP is a threat
+    # CHECK IF THE IP IS A THREAT
     check_ip_threat_udf = spark.udf.register("check_ip_threat", check_ip_threat)
-
     df = df.withColumn("src_threat", check_ip_threat_udf(col("layers.ip.ip_ip_src")))
     df = df.withColumn("dst_threat", check_ip_threat_udf(col("layers.ip.ip_ip_dst")))
 
-    # Anomaly detection
-    # Prepare features for anomaly detection
-    df_features = df.select(
-        "timestamp",
-        "layers.frame.frame_frame_time",
-        "layers.frame.frame_frame_len",
-        "layers.eth.eth_eth_src",
-        "layers.eth.eth_eth_dst",
-        "layers.eth.eth_eth_type",
-        "layers.ip.ip_ip_src",
-        "layers.ip.ip_ip_dst",
-        "layers.ip.ip_ip_proto",
-        "layers.udp.udp_udp_srcport",
-        "layers.udp.udp_udp_dstport",
-        "layers.dns.dns_dns_qry_name",
-        "layers.dns.dns_dns_qry_type",
-        "layers.dns.dns_dns_qry_class"
-    )
+    # APPLICATION LAYER PROTOCOL
+    application_detection_udf = spark.udf.register("application_detection", application_detection)
+    df = df.withColumn("tcp_src_application_protocol", application_detection_udf(col("layers.tcp.tcp_tcp_srcport")))
+    df = df.withColumn("tcp_dst_application_protocol", application_detection_udf(col("layers.tcp.tcp_tcp_dstport")))
+    df = df.withColumn("udp_src_application_protocol", application_detection_udf(col("layers.udp.udp_udp_srcport")))
+    df = df.withColumn("udp_dst_application_protocol", application_detection_udf(col("layers.udp.udp_udp_dstport")))
 
-    udf_ip_to_int = spark.udf.register("ip_to_int", ip_to_int, IntegerType())
-    df_features = df_features.withColumn("ip_ip_src", udf_ip_to_int(col("layers.ip.ip_ip_src")))
-    df_features = df_features.withColumn("ip_ip_dst", udf_ip_to_int(col("layers.ip.ip_ip_dst")))
+    # RESOLVE IP ADDRESSES TO HOSTNAMES
+    resolve_hostname_udf = spark.udf.register("resolve_hostname", resolve_hostname)
+    df = df.withColumn("src_hostname", resolve_hostname_udf(col("layers.ip.ip_ip_src")))
+    df = df.withColumn("dst_hostname", resolve_hostname_udf(col("layers.ip.ip_ip_dst")))
 
-    # Apply anomaly detection model
-    df_features = df_features.withColumn("is_anomaly", detect_anomaly(
-        col("layers.frame.frame_frame_time").cast("double"),
-        col("layers.frame.frame_frame_len").cast("int"),
-        col("layers.eth.eth_eth_src"),
-        col("layers.eth.eth_eth_dst"),
-        col("layers.eth.eth_eth_type"),
-        col("ip_ip_src"),
-        col("ip_ip_dst"),
-        col("layers.ip.ip_ip_proto").cast("int"),
-        col("layers.udp.udp_udp_srcport").cast("int"),
-        col("layers.udp.udp_udp_dstport").cast("int"),
-        col("layers.dns.dns_dns_qry_name"),
-        col("layers.dns.dns_dns_qry_type").cast("int"),
-        col("layers.dns.dns_dns_qry_class").cast("int")
-    ))
+    # ANOMALY DETECTION WITH MODEL
     
+    # Load the model
+    model = load('iforest_model.joblib')
+    # Define the features
+    
+
+    #OUTPUT
+
     # Print to console debug
-    console = df_features.writeStream.outputMode("append").format("console").start()
+    console = df.writeStream.outputMode("append").format("console").start()
     console.awaitTermination()
 
     # Write to Elastic
-    # elastic = df_features.writeStream \
-    #     .option("checkpointLocation", "/tmp/") \
-    #     .format("es") \
-    #     .start(elastic_index)
-    # elastic.awaitTermination()
+    elastic = df.writeStream \
+        .option("checkpointLocation", "/tmp/") \
+        .format("es") \
+        .start(elastic_index)
+    elastic.awaitTermination()
     
 
 if __name__ == "__main__":
