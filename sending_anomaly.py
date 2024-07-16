80bbf97d43536212c4e2ec92f29491b0d8afa839
Leonardo Cantarella <leonardo@cantarella.it>
2024-07-16 12:14:29 +0200

    removed comment

--- sending_anomaly.py
+++ sending_anomaly.py
@@ -1,7 +1,7 @@
 from scapy.all import *
 
 while True:
-    # Define packet parameters (modify as per your anomaly criteria)
+    # Define packet parameters
     packet = IP(dst="127.0.0.1") / UDP(dport=1111) / Raw(b"AnomalyPacket")
 
     # Send the packet
